8578a1ab0a92bca60e90ecc98f122fbe26eda4ae
KarthiSrinivasan25 <karthikeyan25082000@gmail.com>
2024-06-10 18:52:51 +0530

    First Commit

--- assets/php/send_email.php
+++ assets/php/send_email.php
@@ -31,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host = 'smtp.gmail.com'; 
         $mail->SMTPAuth = true; 
         $mail->Username = 'karthikeyan25082000@gmail.com';
-        $mail->Password = 'qtgknrgpovmiydqu'; 
+        $mail->Password = '*************'; 
         $mail->SMTPSecure = 'tls'; 
         $mail->Port = 587; 
 
