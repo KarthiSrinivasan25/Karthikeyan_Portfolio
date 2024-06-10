6e3a85dbd25c8464fa3daa9647f7944c6aedb6a9
KarthiSrinivasan25 <karthikeyan25082000@gmail.com>
2024-06-10 18:48:03 +0530

    sk commit

--- assets/php/send_email.php
+++ assets/php/send_email.php
@@ -1,28 +1,60 @@
 <?php
+require '../../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    $name = htmlspecialchars(trim($_POST["name"]));
+    $email = htmlspecialchars(trim($_POST["email"]));
+    $message = htmlspecialchars(trim($_POST["message"]));
 
-    // Set up email parameters
-    $to = "karthikeyan25082000@gmail.com"; // Replace with your email address
-    $subject = "Message from Portfolio Contact Form";
-    $body = "Name: $name\nEmail: $email\nMessage: $message";
-
-    // Send email
-    if (mail($to, $subject, $body)) {
-        // Email sent successfully
-        header("Location: ../../thankyou.html"); // Redirect to thank you page
+    // Validate form data
+    if (empty($name) || empty($email) || empty($message)) {
+        header("Location: ../../error.html");
         exit;
-    } else {
-        // Failed to send email
-        header("Location: ../../error.html"); // Redirect to error page
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: ../../error.html");
+        exit;
+    }
+
+   
+    $mail = new PHPMailer(true);
+
+    try {
+       
+        $mail->SMTPDebug = 0; 
+        $mail->isSMTP(); 
+        $mail->Host = 'smtp.gmail.com'; 
+        $mail->SMTPAuth = true; 
+        $mail->Username = 'karthikeyan25082000@gmail.com';
+        $mail->Password = 'qtgknrgpovmiydqu'; 
+        $mail->SMTPSecure = 'tls'; 
+        $mail->Port = 587; 
+
+        //Recipients
+        $mail->setFrom('karthikeyan25082000@gmail.com', 'Portfolio');
+        $mail->addAddress('karthikeyansrini25@gmail.com', 'Karthikeyan'); 
+
+        // Content
+        $mail->isHTML(true); 
+        $mail->Subject = '	Message from Portfolio Contact Form ' . $name;
+        $mail->Body    = "Name: " . $name . "<br>Email: " . $email . "<br>Message:" . nl2br($message);
+        $mail->AltBody = "Name: " . $name . "\nEmail: " . $email . "\nMessage:" . $message;
+
+        $mail->send();
+        header("Location: ../../thankyou.html");
+        exit;
+    } catch (Exception $e) {
+        error_log("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        header("Location: ../../error.html");
         exit;
     }
 } else {
-    // If not a POST request, redirect to error page
-    header("Location: ../../error.html"); // Redirect to error page
+    header("Location: ../../error.html");
     exit;
 }
 ?>
