e8de223f476a022a610ae8cd8819e80a21b1dd18
KarthiSrinivasan25 <karthikeyan25082000@gmail.com>
2024-06-13 22:09:38 +0530

    kk commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Define loading sound
+ 
   var loadingSound = new Audio('path/to/loading-sound.mp3');
 
-  // Simulate loading delay (remove this in production)
+
   setTimeout(function() {
-      // Hide loading screen
+
       document.getElementById('loading-screen').style.display = 'none';
-      // Show content
+  
       document.getElementById('content').style.display = 'block';
-      // Pause loading sound
+
       loadingSound.pause();
-  }, 2000); // Adjust the delay time as needed (in milliseconds)
+  }, 2000); 
 });
 
 
